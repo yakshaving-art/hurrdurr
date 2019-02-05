097c53c5fe054245eb8451cd390c32bc952085d5
Pablo Carranza <pcarranza@gmail.com>
2019-02-05 09:34:01 +0100

    Load projects when they are archived too

--- internal/api/gitlab.go
+++ internal/api/gitlab.go
@@ -163,16 +163,16 @@ func LoadFullGitlabState(m GitlabAPIClient) (internal.State, error) {
 				continue
 			}
 
+			variables := make(map[string]string)
 			if project.Archived {
 				// skip archived projects
 				logrus.Debugf("  skipping variables for project '%s' because it's archited", project.PathWithNamespace)
-				continue
-			}
-
-			variables, err := m.fetchProjectVariables(project.PathWithNamespace)
-			if err != nil {
-				errs.Append(fmt.Errorf("failed to fetch project variables for '%s': %s", project.PathWithNamespace, err))
-				continue
+			} else {
+				variables, err = m.fetchProjectVariables(project.PathWithNamespace)
+				if err != nil {
+					errs.Append(fmt.Errorf("failed to fetch project variables for '%s': %s", project.PathWithNamespace, err))
+					continue
+				}
 			}
 
 			logrus.Debugf("  appending project '%s' with it's members", project.PathWithNamespace)
