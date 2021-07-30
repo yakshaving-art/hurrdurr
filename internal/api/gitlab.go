613dcbce26b9fb7f3bab0ece2c1cb4d5595c2b94
Pablo Carranza <pcarranza@gmail.com>
2021-07-30 09:37:07 +0200

    Add locks around map writes, otherwise panics

--- internal/api/gitlab.go
+++ internal/api/gitlab.go
@@ -110,6 +110,9 @@ func LoadFullGitlabState(m GitlabAPIClient) (internal.State, error) {
 
 	errs := errors.New()
 
+	groupsLock := &sync.Mutex{}
+	projectsLock := &sync.Mutex{}
+
 	wg := &sync.WaitGroup{}
 
 	// Create a worker pool that controls concurrency tightly, with as many slots are concurrency is enabled
@@ -159,6 +162,9 @@ func LoadFullGitlabState(m GitlabAPIClient) (internal.State, error) {
 						errs.Append(fmt.Errorf("Failed fetching group variables (took %s): %s", time.Since(jobTime), err))
 						return
 					}
+
+					groupsLock.Lock()
+					defer groupsLock.Unlock()
 
 					groups[group.FullPath] = GitlabGroup{
 						fullpath:   group.FullPath,
@@ -219,6 +225,9 @@ func LoadFullGitlabState(m GitlabAPIClient) (internal.State, error) {
 					}
 
 					logrus.Tracef("appending project '%s' with its members (took %s)", project.PathWithNamespace, time.Since(jobTime))
+
+					projectsLock.Lock()
+					defer projectsLock.Unlock()
 
 					projects[project.PathWithNamespace] = GitlabProject{
 						fullpath:   project.PathWithNamespace,
